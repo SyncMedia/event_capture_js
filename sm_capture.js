8712043e2d7f9893473b1a81fae94c44c9e21f16
Pankaj Soni <pankajsoni19@live.com>
2020-10-05 16:13:46 +0530

    event id

--- sm_capture.js
+++ sm_capture.js
@@ -1,3 +1,10 @@
+/*
+* https://github.com/uuidjs/uuid
+* uuidv4-8.1.0.min.js
+*/
+
+!function(t,e){"object"==typeof exports&&"undefined"!=typeof module?module.exports=e():"function"==typeof define&&define.amd?define(e):(t=t||self).uuidv4=e()}(this,(function(){"use strict";var t="undefined"!=typeof crypto&&crypto.getRandomValues&&crypto.getRandomValues.bind(crypto)||"undefined"!=typeof msCrypto&&"function"==typeof msCrypto.getRandomValues&&msCrypto.getRandomValues.bind(msCrypto),e=new Uint8Array(16);function n(){if(!t)throw new Error("crypto.getRandomValues() not supported. See https://github.com/uuidjs/uuid#getrandomvalues-not-supported");return t(e)}for(var o=[],r=0;r<256;++r)o.push((r+256).toString(16).substr(1));return function(t,e,r){"string"==typeof t&&(e="binary"===t?new Uint8Array(16):null,t=null);var u=(t=t||{}).random||(t.rng||n)();if(u[6]=15&u[6]|64,u[8]=63&u[8]|128,e){for(var i=r||0,d=0;d<16;++d)e[i+d]=u[d];return e}return function(t,e){var n=e||0,r=o;return(r[t[n+0]]+r[t[n+1]]+r[t[n+2]]+r[t[n+3]]+"-"+r[t[n+4]]+r[t[n+5]]+"-"+r[t[n+6]]+r[t[n+7]]+"-"+r[t[n+8]]+r[t[n+9]]+"-"+r[t[n+10]]+r[t[n+11]]+r[t[n+12]]+r[t[n+13]]+r[t[n+14]]+r[t[n+15]]).toLowerCase()}(u)}}));
+
 /**
  *
  * @auther SM@K<smali.kazmi@hotmail.com>
@@ -198,6 +205,14 @@ const DEBUG = (window.location.hostname == "localhost");
     }());
 
     let geo_data = () => {
+        let cached = localStorage.getItem("sm.geo.cache");
+
+        if (!!cached) {
+            return new Promise((resolve, reject) => {
+                resolve(JSON.parse(cached));
+            });
+        }
+
         let args = {
               method: "GET",
               mode: "cors", // same-origin, no-cors
@@ -209,7 +224,11 @@ const DEBUG = (window.location.hostname == "localhost");
 
         return fetch('https://ipinfo.io/json?token=f6d5b4490ab073', args)
             .then((resp) => resp.json())
-            .catch((error) => { return {}; });
+            .catch((error) => { return {}; })
+            .then((data) => {
+                localStorage.setItem("sm.geo.cache", JSON.stringify(data));
+                return data;
+            });
     };
 
     let config = {
@@ -274,7 +293,13 @@ const DEBUG = (window.location.hostname == "localhost");
             return;
         }
 
-        let json = {index: key, payload: "", config: config};
+        let json = {
+            index: key,
+            payload: "",
+            config: config,
+            timestamp: new Date().getTime(),
+            event_id: uuidv4()
+        };
 
         if (typeof(payload) == "object") {
             json.payload = JSON.stringify(payload);
